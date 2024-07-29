fe768e197b463f5059ff5eaa92e3a5ee0bebdb0c
IceLink1 <163158076+IceLink1@users.noreply.github.com>
2024-07-29 14:24:59 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+import express from 'express'
+import fs from 'fs'
+import multer from 'multer'
+import cors from 'cors'
+
+import mongoose from 'mongoose'
+
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js'
+
+import { handleValidationErrors, checkAuth } from './utils/index.js'
+
+import { UserController, PostController } from './controllers/index.js'
+
+mongoose.connect("mongodb+srv://icelink39:9210369979Ash@cluster0.sthnhtn.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0").then(() => console.log('DB ok')).catch(err => console.log('DB error :', err))//process.env.MONGODB_URI
+
+const app = express()
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads')
+    }
+    cb(null, 'uploads')
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname)
+  },
+})
+
+const upload = multer({ storage })
+
+app.use(express.json())
+app.use(cors())
+app.use('/uploads', express.static('uploads'))
+
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login)
+app.post('/auth/register', registerValidation, handleValidationErrors, UserController.register)
+app.get('/auth/me', checkAuth, UserController.getMe)
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  })
+})
+
+app.get('/tags', PostController.getLastTags)
+
+app.get('/posts', PostController.getAll)
+app.get('/posts/tags', PostController.getLastTags)
+app.get('/posts/:id', PostController.getOne)
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create)
+app.delete('/posts/:id', checkAuth, PostController.remove)
+app.patch('/posts/:id',checkAuth,postCreateValidation,handleValidationErrors,PostController.update,)
+
+app.listen(process.env.PORT || 4444, (err) => {
+  if (err) {
+    return console.log(err)
+  }
+
+  console.log('Server OK')
+})
---
+++ models/User.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    avatarUrl:{
+      type: String,
+      default:"https://media.istockphoto.com/id/1337144146/vector/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=BIbFwuv7FxTWvh5S3vB6bkT0Qv8Vn8N5Ffseq84ClGI="
+    } 
+      
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model('User', UserSchema);
